3df79dafc18064efe47a2a63de5768574f3824aa
picklewagon <picklewagon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-30 23:07:45 +0000

    git-svn-id: http://plugins.svn.wordpress.org/new-user-approve/branches/1.3@269598 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- new-user-approve.php
+++ new-user-approve.php
@@ -29,12 +29,12 @@ if (!class_exists('pw_new_user_approve')) {
 		/**
 		 * @var string The options string name for this plugin
 		 */
-		var $optionsName = 'pw_new_user_approve_options';
+		var $options_name = 'pw_new_user_approve_options';
 
 		/**
-		 * @var string $localizationDomain Domain used for localization
+		 * @var string $plugin_id unique identifier used for localization and other functions
 		 */
-		var $localizationDomain = 'new-user-approve';
+		var $plugin_id = 'new-user-approve';
 
 		/**
 		 * @var string $pluginurl The url to this plugin
@@ -65,7 +65,7 @@ if (!class_exists('pw_new_user_approve')) {
 		function __construct() {
 			// Load up the localization file if we're using WordPress in a different language
 			// Just drop it in this plugin's "localization" folder and name it "new-user-approve-[value in wp-config].mo"
-			load_plugin_textdomain($this->localizationDomain, false, dirname(plugin_basename(__FILE__)) . '/localization');
+			load_plugin_textdomain($this->plugin_id, false, dirname(plugin_basename(__FILE__)) . '/localization');
 
 			// Constants setup
 			$this->pluginurl  = WP_PLUGIN_URL . '/' . dirname(plugin_basename(__FILE__)).'/';
@@ -77,6 +77,7 @@ if (!class_exists('pw_new_user_approve')) {
 			// Actions
 			add_action('admin_menu',          array(&$this, 'admin_menu_link'));
 			add_action('admin_footer',        array(&$this, 'admin_scripts_footer'));
+			add_action('admin_init',          array(&$this, 'admin_init'));
 			add_action('init',                array(&$this, 'init'));
 			add_action('register_post',       array(&$this, 'send_approval_email'), 10, 3);
 			add_action('init',                array(&$this, 'process_input'));
@@ -91,11 +92,11 @@ if (!class_exists('pw_new_user_approve')) {
 		 */
 		function get_options() {
 			// Don't forget to set up the default options
-			if (!$theOptions = get_option($this->optionsName)) {
+			if (!$theOptions = get_option($this->options_name)) {
 				$theOptions = array(
 					'default' => 'options'
 				);
-				update_option($this->optionsName, $theOptions);
+				update_option($this->options_name, $theOptions);
 			}
 			$this->options = $theOptions;
 		}
@@ -104,17 +105,17 @@ if (!class_exists('pw_new_user_approve')) {
 		 * @desc Saves the admin options to the database.
 		 */
 		function save_admin_options(){
-			update_option($this->optionsName, $this->options);
+			update_option($this->options_name, $this->options);
 		}
 
 		/**
 		 * @desc Adds the options subpanel
 		 */
 		function admin_menu_link() {
-			add_submenu_page('users.php', __('Approve New Users', $this->localizationDomain), __('Approve New Users', $this->localizationDomain), 'edit_users', basename(__FILE__), array(&$this, 'approve_admin'));
+			add_submenu_page('users.php', __('Approve New Users', $this->plugin_id), __('Approve New Users', $this->plugin_id), 'edit_users', basename(__FILE__), array(&$this, 'approve_admin'));
 			add_filter('plugin_action_links', array(&$this, 'filter_plugin_actions'), 10, 2);
 			
-			add_options_page(__('Approve New Users', $this->localizationDomain), __('Approve New Users', $this->localizationDomain), 'manage_options', $this->localizationDomain, array(&$this, 'options_page'));
+			add_options_page(__('Approve New Users', $this->plugin_id), __('Approve New Users', $this->plugin_id), 'manage_options', $this->plugin_id, array(&$this, 'options_page'));
 		}
 
 		/**
@@ -127,12 +128,26 @@ if (!class_exists('pw_new_user_approve')) {
 			}
 
 			if ($file == $this_plugin){
-				$settings_link = '<a href="users.php?page=' . basename(__FILE__) . '">' . __('Settings', $this->localizationDomain) . '</a>';
+				$settings_link = '<a href="users.php?page=' . basename(__FILE__) . '">' . __('Settings', $this->plugin_id) . '</a>';
 				array_unshift( $links, $settings_link ); // before other links
 			}
 			return $links;
 		}
 
+		function admin_init() {
+			register_setting($this->options_name, $this->plugin_id, array(&$this, 'options_validate'));
+			add_settings_section($this->plugin_id . 'main', 'Main Settings', array(&$this, 'main_section_text'), $this->plugin_id);
+			//add_settings_field('plugin_text_string', 'Plugin Text Input', 'plugin_setting_string', 'plugin', 'plugin_main');
+		}
+		
+		function options_validate() {
+			
+		}
+		
+		function main_section_text() {
+			
+		}
+		
 		function admin_scripts_footer() {
 			global $wp_db_version;
 			
@@ -170,7 +185,7 @@ if (!class_exists('pw_new_user_approve')) {
 			}
 ?>
 			<div>
-				<p><span style="font-weight:bold;"><a href="users.php?page=<?php print basename(__FILE__) ?>"><?php _e('Users', $this->localizationDomain) ?></a></span>: 
+				<p><span style="font-weight:bold;"><a href="users.php?page=<?php print basename(__FILE__) ?>"><?php _e('Users', $this->plugin_id) ?></a></span>: 
 				<?php foreach($user_status as $status =>$count) print "$count $status&nbsp;&nbsp;"; ?>
 				</p>
 			</div>
@@ -209,18 +224,18 @@ if (!class_exists('pw_new_user_approve')) {
 			}
 			
 			if (isset($_GET['user']) && isset($_GET['status'])) {
-				echo '<div id="message" class="updated fade"><p>'.__('User successfully updated.', $this->localizationDomain).'</p></div>';
+				echo '<div id="message" class="updated fade"><p>'.__('User successfully updated.', $this->plugin_id).'</p></div>';
 			}
 ?>
 			<div class="wrap">
-				<h2><?php _e('User Registration Approval', $this->localizationDomain) ?></h2>
+				<h2><?php _e('User Registration Approval', $this->plugin_id) ?></h2>
 				
-				<h3><?php _e('User Management', $this->localizationDomain) ?></h3>
+				<h3><?php _e('User Management', $this->plugin_id) ?></h3>
 				<div id="pw_approve_tabs">
 					<ul>
-						<li><a href="#pw_pending_users"><span><?php _e('Users Pending Approval', $this->localizationDomain) ?></span></a></li>
-						<li><a href="#pw_approved_users"><span><?php _e('Approved Users', $this->localizationDomain) ?></span></a></li>
-						<li><a href="#pw_denied_users"><span><?php _e('Denied Users', $this->localizationDomain) ?></span></a></li>
+						<li><a href="#pw_pending_users"><span><?php _e('Users Pending Approval', $this->plugin_id) ?></span></a></li>
+						<li><a href="#pw_approved_users"><span><?php _e('Approved Users', $this->plugin_id) ?></span></a></li>
+						<li><a href="#pw_denied_users"><span><?php _e('Denied Users', $this->plugin_id) ?></span></a></li>
 					</ul>
 					<div id="pw_pending_users">
 						<?php $this->approve_table($user_status, 'pending', true, true); ?>
@@ -245,14 +260,14 @@ if (!class_exists('pw_new_user_approve')) {
 <table class="widefat">
 	<thead>
 		<tr class="thead">
-			<th><?php _e('ID', $this->localizationDomain) ?></th>
-			<th><?php _e('Username', $this->localizationDomain) ?></th>
-			<th><?php _e('Name', $this->localizationDomain) ?></th>
-			<th><?php _e('E-mail', $this->localizationDomain) ?></th>
+			<th><?php _e('ID', $this->plugin_id) ?></th>
+			<th><?php _e('Username', $this->plugin_id) ?></th>
+			<th><?php _e('Name', $this->plugin_id) ?></th>
+			<th><?php _e('E-mail', $this->plugin_id) ?></th>
 		<?php if ($approve && $deny) { ?>
-			<th colspan="2" style="text-align: center"><?php _e('Actions', $this->localizationDomain) ?></th>
+			<th colspan="2" style="text-align: center"><?php _e('Actions', $this->plugin_id) ?></th>
 		<?php } else { ?>
-			<th style="text-align: center"><?php _e('Actions', $this->localizationDomain) ?></th>
+			<th style="text-align: center"><?php _e('Actions', $this->plugin_id) ?></th>
 		<?php } ?>
 		</tr>
 	</thead>
@@ -286,12 +301,12 @@ if (!class_exists('pw_new_user_approve')) {
 					<td><?php echo $user->ID; ?></td>
 					<td><?php echo $avatar." ".$edit; ?></td>
 					<td><?php echo $user->first_name." ".$user->last_name; ?></td>
-					<td><a href="mailto:<?php echo $user->user_email; ?>" title="<?php _e('email:', $this->localizationDomain) ?> <?php echo $user->user_email; ?>"><?php echo $user->user_email; ?></a></td>
+					<td><a href="mailto:<?php echo $user->user_email; ?>" title="<?php _e('email:', $this->plugin_id) ?> <?php echo $user->user_email; ?>"><?php echo $user->user_email; ?></a></td>
 					<?php if ($approve) { ?>
-					<td align="center"><a href="<?php echo $approve_link; ?>" title="<?php _e('Approve', $this->localizationDomain) ?> <?php echo $user->user_login; ?>"><?php _e('Approve', $this->localizationDomain) ?></a></td>
+					<td align="center"><a href="<?php echo $approve_link; ?>" title="<?php _e('Approve', $this->plugin_id) ?> <?php echo $user->user_login; ?>"><?php _e('Approve', $this->plugin_id) ?></a></td>
 					<?php } ?>
 					<?php if ($deny) { ?>
-					<td align="center"><a href="<?php echo $deny_link; ?>" title="<?php _e('Deny', $this->localizationDomain) ?> <?php echo $user->user_login; ?>"><?php _e('Deny', $this->localizationDomain) ?></a></td>
+					<td align="center"><a href="<?php echo $deny_link; ?>" title="<?php _e('Deny', $this->plugin_id) ?> <?php echo $user->user_login; ?>"><?php _e('Deny', $this->plugin_id) ?></a></td>
 					<?php } ?>
 				</tr><?php
 				$row++;
@@ -303,14 +318,14 @@ if (!class_exists('pw_new_user_approve')) {
 			} else {
 				$status_i18n = $status;
 				if ($status == 'approved') {
-					$status_i18n = __('approved', $this->localizationDomain);
+					$status_i18n = __('approved', $this->plugin_id);
 				} else if ($status == 'denied') {
-					$status_i18n = __('denied', $this->localizationDomain);
+					$status_i18n = __('denied', $this->plugin_id);
 				} else if ($status == 'pending') {
-					$status_i18n = __('pending', $this->localizationDomain);
+					$status_i18n = __('pending', $this->plugin_id);
 				}
 					
-				echo '<p>'.sprintf(__('There are no users with a status of %s', $this->localizationDomain), $status_i18n).'</p>';
+				echo '<p>'.sprintf(__('There are no users with a status of %s', $this->plugin_id), $status_i18n).'</p>';
 			}
 		}
 		
@@ -322,16 +337,16 @@ if (!class_exists('pw_new_user_approve')) {
 				/* check if already exists */
 				$user_data = get_userdatabylogin($user_login);
         		if (!empty($user_data)){
-					$errors->add('registration_required' , __('User name already exists', $this->localizationDomain), 'message');
+					$errors->add('registration_required' , __('User name already exists', $this->plugin_id), 'message');
         		} else {
 					/* send email to admin for approval */
-        			$message  = sprintf(__('%1$s (%2$s) has requested a username at %3$s', $this->localizationDomain), $user_login, $user_email, get_option('blogname')) . "\r\n\r\n";
+        			$message  = sprintf(__('%1$s (%2$s) has requested a username at %3$s', $this->plugin_id), $user_login, $user_email, get_option('blogname')) . "\r\n\r\n";
 					$message .= get_option('siteurl') . "\r\n\r\n";
-					$message .= sprintf(__('To approve or deny this user access to %s go to', $this->localizationDomain), get_option('blogname')) . "\r\n\r\n";
+					$message .= sprintf(__('To approve or deny this user access to %s go to', $this->plugin_id), get_option('blogname')) . "\r\n\r\n";
 					$message .= get_option('siteurl') . "/wp-admin/users.php?page=".basename(__FILE__)."\r\n";
 				
 					// send the mail
-					@wp_mail(get_option('admin_email'), sprintf(__('[%s] User Approval', $this->localizationDomain), get_option('blogname')), $message);
+					@wp_mail(get_option('admin_email'), sprintf(__('[%s] User Approval', $this->plugin_id), get_option('blogname')), $message);
 					
 					// create the user
 					$user_pass = wp_generate_password();
@@ -370,13 +385,13 @@ if (!class_exists('pw_new_user_approve')) {
 			$user_email = stripslashes($user->user_email);
 			
 			// format the message
-			$message  = sprintf(__('You have been approved to access %s', $this->localizationDomain), get_option('blogname')) . "\r\n";
-			$message .= sprintf(__('Username: %s', $this->localizationDomain), $user_login) . "\r\n";
-			$message .= sprintf(__('Password: %s', $this->localizationDomain), $new_pass) . "\r\n";
+			$message  = sprintf(__('You have been approved to access %s', $this->plugin_id), get_option('blogname')) . "\r\n";
+			$message .= sprintf(__('Username: %s', $this->plugin_id), $user_login) . "\r\n";
+			$message .= sprintf(__('Password: %s', $this->plugin_id), $new_pass) . "\r\n";
 			$message .= get_option('siteurl') . "/wp-login.php\r\n";
 		
 			// send the mail
-			@wp_mail($user_email, sprintf(__('[%s] Registration Approved', $this->localizationDomain), get_option('blogname')), $message);
+			@wp_mail($user_email, sprintf(__('[%s] Registration Approved', $this->plugin_id), get_option('blogname')), $message);
 			
 			// change usermeta tag in database to approved
 			update_usermeta($user->ID, 'pw_user_status', 'approved');
@@ -395,10 +410,10 @@ if (!class_exists('pw_new_user_approve')) {
 			$user_email = stripslashes($user->user_email);
 			
 			// format the message
-			$message = sprintf(__('You have been denied access to %s', $this->localizationDomain), get_option('blogname'));
+			$message = sprintf(__('You have been denied access to %s', $this->plugin_id), get_option('blogname'));
 		
 			// send the mail
-			@wp_mail($user_email, sprintf(__('[%s] Registration Denied', $this->localizationDomain), get_option('blogname')), $message);
+			@wp_mail($user_email, sprintf(__('[%s] Registration Denied', $this->plugin_id), get_option('blogname')), $message);
 			
 			// change usermeta tag in database to denied
 			update_usermeta($user->ID, 'pw_user_status', 'denied');
@@ -411,13 +426,13 @@ if (!class_exists('pw_new_user_approve')) {
 			if ( $errors->get_error_code() )
 				return $errors;
 			
-			$message  = sprintf(__('An email has been sent to the site administrator. The administrator will review the information that has been submitted and either approve or deny your request.', $this->localizationDomain));
-			$message .= sprintf(__('You will receive an email with instructions on what you will need to do next. Thanks for your patience.', $this->localizationDomain));
+			$message  = sprintf(__('An email has been sent to the site administrator. The administrator will review the information that has been submitted and either approve or deny your request.', $this->plugin_id));
+			$message .= sprintf(__('You will receive an email with instructions on what you will need to do next. Thanks for your patience.', $this->plugin_id));
 			
 			$errors->add('registration_required', $message, 'message');
 			
 			if (function_exists('login_header')) {
-				login_header(__('Pending Approval', $this->localizationDomain), '<p class="message register">' . __("Registration successful.", $this->localizationDomain) . '</p>', $errors);
+				login_header(__('Pending Approval', $this->plugin_id), '<p class="message register">' . __("Registration successful.", $this->plugin_id) . '</p>', $errors);
 			}
 			
 			echo "<body></html>";
@@ -466,12 +481,12 @@ if (!class_exists('pw_new_user_approve')) {
 		
 		function welcome_user($message) {
 			if (!isset($_GET['action'])) {
-				$inside = sprintf(__('Welcome to %s. This site is accessible to approved users only. To be approved, you must first register.', $this->localizationDomain), get_option('blogname'));
+				$inside = sprintf(__('Welcome to %s. This site is accessible to approved users only. To be approved, you must first register.', $this->plugin_id), get_option('blogname'));
 				$message .= '<p class="message">' . $inside . '</p>';
 			}
 			
 			if ($_GET['action'] == 'register' && !$_POST) {
-				$inside = sprintf(__('After you register, your request will be sent to the site administrator for approval. You will then receive an email with further instructions.', $this->localizationDomain));
+				$inside = sprintf(__('After you register, your request will be sent to the site administrator for approval. You will then receive an email with further instructions.', $this->plugin_id));
 				$message .= '<p class="message">' . $inside . '</p>';
 			}
 			
@@ -486,6 +501,17 @@ if (!class_exists('pw_new_user_approve')) {
 		}
 		
 		function options_page() {
+?>
+<div class="wrap">
+	<h2>Approve New Users Options</h2>
+	<form action="options.php" method="post">
+	<?php settings_fields($this->options_name); ?>
+	<?php do_settings_sections($this->plugin_id); ?>
+	<input name="Submit" type="submit" value="<?php esc_attr_e('Save Changes'); ?>" />
+	</form>
+</div>
+
+<?php
 		}
 	} // End Class
 } // End if class exists statement
