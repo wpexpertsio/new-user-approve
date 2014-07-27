34e4e3bf1eb26914492c564b075c2dd37feacec0
Josh Harrison <josh@picklewagon.com>
2014-07-26 18:30:25 -0600

    add feedback and support metaboxes

--- includes/admin-approve.php
+++ includes/admin-approve.php
@@ -217,6 +217,8 @@ class pw_new_user_approve_admin_approve {
 	public function add_meta_boxes() {
 		add_meta_box( 'nua-approve-admin', __( 'Approve Users', 'new-user-approve' ), array( $this, 'metabox_main' ), 'users_page_new-user-approve-admin', 'main', 'high' );
 		add_meta_box( 'nua-updates', __( 'Updates', 'new-user-approve' ), array( $this, 'metabox_updates' ), 'users_page_new-user-approve-admin', 'side' );
+		add_meta_box( 'nua-support', __( 'Support', 'new-user-approve' ), array( $this, 'metabox_support' ), 'users_page_new-user-approve-admin', 'side' );
+		add_meta_box( 'nua-feedback', __( 'Feedback', 'new-user-approve' ), array( $this, 'metabox_feedback' ), 'users_page_new-user-approve-admin', 'side' );
 	}
 
 	public function metabox_main() {
@@ -249,6 +251,14 @@ elseif ( $active_tab == 'denied_users' ) : ?>
 
 	public function metabox_updates() {
 		echo 'updates';
+	}
+
+	public function metabox_support() {
+		echo 'support';
+	}
+
+	public function metabox_feedback() {
+		echo 'feedback';
 	}
 
 }
