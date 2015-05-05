af169cfb62bd609534e6ddbff5992270d0228cdc
Josh Harrison <josh@picklewagon.com>
2015-05-05 01:20:33 -0700

    Add phplint task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,14 @@
 var gulp = require( 'gulp' );
+var phplint = require('phplint').lint;
+
+gulp.task( 'phplint', function( cb ) {
+    phplint( [ './**/*.php', '!./node_modules/**/*' ], { limit: 10 }, function ( err, stdout, stderr ) {
+        if ( err ) {
+            cb( err );
+        } else {
+            cb();
+        }
+    } );
+} );
 
 gulp.task( 'default', [] );
