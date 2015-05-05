cb6d75546d8fc49f03cedeee56a3b879ace9b42f
Josh Harrison <josh@picklewagon.com>
2015-05-05 00:20:18 -0700

    Update bootstrap to run unit tests.

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -9,50 +9,90 @@ DB_NAME=$1
 DB_USER=$2
 DB_PASS=$3
 DB_HOST=${4-localhost}
-WP_VERSION=${5-master}
+WP_VERSION=${5-latest}
+
+WP_TESTS_DIR=${WP_TESTS_DIR-/tmp/wordpress-tests-lib}
+WP_CORE_DIR=${WP_CORE_DIR-/tmp/wordpress/}
 
 set -ex
 
-# set up a WP install
-WP_CORE_DIR=/tmp/wordpress/
-mkdir -p $WP_CORE_DIR
-wget -nv -O /tmp/wordpress.tar.gz https://github.com/WordPress/WordPress/tarball/$WP_VERSION
-tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
+download() {
+    if [ `which curl` ]; then
+        curl -s "$1" > "$2";
+    elif [ `which wget` ]; then
+        wget -nv -O "$2" "$1"
+    fi
+}
 
-# set up testing suite
-svn co --ignore-externals --quiet http://unit-tests.svn.wordpress.org/trunk/ $WP_TESTS_DIR
+install_wp() {
 
-# portable in-place argument for both GNU sed and Mac OSX sed
-if [[ $(uname -s) == 'Darwin' ]]; then
-  ioption='-i ""'
-else
-  ioption='-i'
-fi
+	if [ -d $WP_CORE_DIR ]; then
+		return;
+	fi
 
-# generate testing config file
-cd $WP_TESTS_DIR
-cp wp-tests-config-sample.php wp-tests-config.php
-sed $ioption "s:dirname( __FILE__ ) . '/wordpress/':'$WP_CORE_DIR':" wp-tests-config.php
-sed $ioption "s/yourdbnamehere/$DB_NAME/" wp-tests-config.php
-sed $ioption "s/yourusernamehere/$DB_USER/" wp-tests-config.php
-sed $ioption "s/yourpasswordhere/$DB_PASS/" wp-tests-config.php
-sed $ioption "s|localhost|${DB_HOST}|" wp-tests-config.php
+	mkdir -p $WP_CORE_DIR
 
-# parse DB_HOST for port or socket references
-PARTS=(${DB_HOST//\:/ })
-DB_HOSTNAME=${PARTS[0]};
-DB_SOCK_OR_PORT=${PARTS[1]};
-EXTRA=""
+	if [ $WP_VERSION == 'latest' ]; then 
+		local ARCHIVE_NAME='latest'
+	else
+		local ARCHIVE_NAME="wordpress-$WP_VERSION"
+	fi
 
-if ! [ -z $DB_HOSTNAME ] ; then
-  if [[ "$DB_SOCK_OR_PORT" =~ ^[0-9]+$ ]] ; then
-    EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT --protocol=tcp"
-  elif ! [ -z $DB_SOCK_OR_PORT ] ; then
-    EXTRA=" --socket=$DB_SOCK_OR_PORT"
-  elif ! [ -z $DB_HOSTNAME ] ; then
-    EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
-  fi
-fi
+	download https://wordpress.org/${ARCHIVE_NAME}.tar.gz  /tmp/wordpress.tar.gz
+	tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 
-# create database
-mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
+	download https://raw.github.com/markoheijnen/wp-mysqli/master/db.php $WP_CORE_DIR/wp-content/db.php
+}
+
+install_test_suite() {
+	# portable in-place argument for both GNU sed and Mac OSX sed
+	if [[ $(uname -s) == 'Darwin' ]]; then
+		local ioption='-i .bak'
+	else
+		local ioption='-i'
+	fi
+
+	# set up testing suite if it doesn't yet exist
+	if [ ! "$(ls -A $WP_TESTS_DIR)" ]; then
+		# set up testing suite
+		mkdir -p $WP_TESTS_DIR
+		svn co --quiet http://develop.svn.wordpress.org/trunk/tests/phpunit/includes/ $WP_TESTS_DIR
+	fi
+
+	cd $WP_TESTS_DIR
+
+	if [ ! -f wp-tests-config.php ]; then
+		download https://develop.svn.wordpress.org/trunk/wp-tests-config-sample.php wp-tests-config.php
+		sed $ioption "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR':" wp-tests-config.php
+		sed $ioption "s/youremptytestdbnamehere/$DB_NAME/" wp-tests-config.php
+		sed $ioption "s/yourusernamehere/$DB_USER/" wp-tests-config.php
+		sed $ioption "s/yourpasswordhere/$DB_PASS/" wp-tests-config.php
+		sed $ioption "s|localhost|${DB_HOST}|" wp-tests-config.php
+	fi
+
+}
+
+install_db() {
+	# parse DB_HOST for port or socket references
+	local PARTS=(${DB_HOST//\:/ })
+	local DB_HOSTNAME=${PARTS[0]};
+	local DB_SOCK_OR_PORT=${PARTS[1]};
+	local EXTRA=""
+
+	if ! [ -z $DB_HOSTNAME ] ; then
+		if [ $(echo $DB_SOCK_OR_PORT | grep -e '^[0-9]\{1,\}$') ]; then
+			EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT --protocol=tcp"
+		elif ! [ -z $DB_SOCK_OR_PORT ] ; then
+			EXTRA=" --socket=$DB_SOCK_OR_PORT"
+		elif ! [ -z $DB_HOSTNAME ] ; then
+			EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
+		fi
+	fi
+
+	# create database
+	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
+}
+
+install_wp
+install_test_suite
+install_db
--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #
 
-composer install
+# ./bin/install-wp-tests.sh wordpress-unit-tests wp wp
+# WP_DEVELOP_DIR=/srv/www/wordpress-develop phpunit
