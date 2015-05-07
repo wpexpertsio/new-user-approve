68cd8ce86fb0d4435acb54b341de0a0ddc1e16fb
Josh Harrison <josh@picklewagon.com>
2015-05-06 22:53:31 -0600

    Add a task for phpunit.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp = require( 'gulp' );
-var phplint = require('phplint').lint;
+var gulp    = require( 'gulp' );
+var phplint = require( 'phplint' ).lint;
+var phpunit = require( 'gulp-phpunit' );
 
 gulp.task( 'phplint', function( cb ) {
     phplint( [ './**/*.php', '!./node_modules/**/*' ], { limit: 10 }, function ( err, stdout, stderr ) {
@@ -10,5 +11,12 @@ gulp.task( 'phplint', function( cb ) {
         }
     } );
 } );
+
+gulp.task( 'phpunit', function() {
+    return gulp.src( 'phpunit.xml' )
+        .pipe(phpunit('phpunit'))
+        .on('error', console.error('TESTS FAILED:\nYou killed someones baby!'))
+        .pipe(function () { console.log('TESTS PASSED:\nAwesome you rock!'); });
+});
 
 gulp.task( 'default', [] );
