55f363a44e3fb479844520c6970354e514c65018
Josh Harrison <josh@picklewagon.com>
2015-09-23 20:00:18 -0600

    Don't phone home for support content.

--- includes/admin-approve.php
+++ includes/admin-approve.php
@@ -219,7 +219,7 @@ class pw_new_user_approve_admin_approve {
 	public function add_meta_boxes() {
 		add_meta_box( 'nua-approve-admin', __( 'Approve Users', 'new-user-approve' ), array( $this, 'metabox_main' ), 'users_page_new-user-approve-admin', 'main', 'high' );
 		add_meta_box( 'nua-updates', __( 'Updates', 'new-user-approve' ), array( $this, 'metabox_updates' ), 'users_page_new-user-approve-admin', 'side', 'default' );
-		add_meta_box( 'nua-support', __( 'Support', 'new-user-approve' ), array( $this, 'metabox_ajax' ), 'users_page_new-user-approve-admin', 'side', 'default', array( 'url' => 'http://newuserapprove.com/wp-json/posts/54' ) );
+		add_meta_box( 'nua-support', __( 'Support', 'new-user-approve' ), array( $this, 'metabox_support' ), 'users_page_new-user-approve-admin', 'side', 'default' );
 		add_meta_box( 'nua-feedback', __( 'Feedback', 'new-user-approve' ), array( $this, 'metabox_ajax' ), 'users_page_new-user-approve-admin', 'side', 'default', array( 'url' => 'http://newuserapprove.com/wp-json/posts/56' ) );
 	}
 
@@ -252,7 +252,7 @@ elseif ( $active_tab == 'denied_users' ) : ?>
 	}
 
 	public function metabox_updates() {
-		?>
+?>
 		I have created a site to help with the support of this plugin. Check it out at <a title="newuserapprove.com" href="https://newuserapprove.com/" target="_blank">newuserapprove.com</a>.
 
 		Please signup for the mailing list to keep up to date.
@@ -270,7 +270,16 @@ elseif ( $active_tab == 'denied_users' ) : ?>
 				</div>
 			</form>
 		</div>
-		<?php
+<?php
+	}
+
+	public function metabox_support() {
+?>
+		<p>If you haven't already, check out the <a href="https://wordpress.org/plugins/new-user-approve/faq/" target="_blank">Frequently Asked Questions</a>.</p>
+		<p>Still not fixed? Please <a href="https://wordpress.org/support/plugin/new-user-approve" target="_blank">start a support topic</a> and I or someone from the community will be able to assist you.</p>
+<?php
+	}
+
 	}
 
 	public function metabox_ajax( $post, $metabox = array() ) {
